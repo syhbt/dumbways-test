d2dbb538d8b1e8c74a20a08f0310ac1b8665e995
Rama <syahbita.rama@gmail.com>
2021-04-10 15:48:19 +0700

    Dumbwyas klontong-2

--- 1.php
+++ 1.php
@@ -1,29 +1,59 @@
-<?php
+​<?php
 
 function HitungVoucher($voucher, $total)
 {
     $diskon = $total * (21.1 / 100);
+    $diskon2 = $total * (30 / 100);
+    $maks_diskon2 = 40000;
     $maks_diskon = 20000;
 
-    if ($diskon >= $maks_diskon) {
-            $total = $total - $maks_diskon; 
-            echo "Uang yang harus dibayar : ".$total."<br>";
-            echo "Diskon : {$maks_diskon}<br>";
-			$kembalian = $total + $maks_diskon - $total;
-            echo "Kembalian : {$kembalian}";
+    if ($voucher == "DumbwaysJos") {
+
+        if ($diskon >= $maks_diskon) {
+                $total = $total - $maks_diskon; 
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : {$maks_diskon}<br>";
+                $kembalian = $total + $maks_diskon - $total;
+                echo "Kembalian : {$kembalian}";
+        }
+        elseif ($total >= 50000) {
+                $total = $total - $diskon;
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : {$diskon}<br>";
+                echo "Kembalian : {$diskon}";
+        }
+        elseif ($total < 50000) {
+                $total = $total;
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : 0<br>";
+                echo "Kembalian : 0";
+        }
     }
-    elseif ($total >= 50000) {
-            $total = $total - $diskon;
-            echo "Uang yang harus dibayar : ".$total."<br>";
-            echo "Diskon : {$diskon}<br>";
-            echo "Kembalian : {$diskon}";
+    elseif ($voucher == "DumbwaysMantap") {
+
+        if ($diskon2 >= $maks_diskon2) {
+                $total = $total - $maks_diskon2; 
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : $maks_diskon2<br>";
+                $kembalian = $total + $maks_diskon2 - $total;
+                echo "Kembalian : {$kembalian}";
+        }
+        elseif ($total >= 80000) {
+                $total = $total - $diskon2;
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : $diskon2<br>";
+                echo "Kembalian : $diskon2";
+        }
+        elseif ($total < 80000) {
+                $total = $total;
+                echo "Uang yang harus dibayar : ".$total."<br>";
+                echo "Diskon : 0<br>";
+                echo "Kembalian : 0";
+        }
     }
-    elseif ($total < 50000) {
-            $total = $total;
-            echo "Uang yang harus dibayar : ".$total."<br>";
-            echo "Diskon : 0<br>";
-            echo "Kembalian : 0";
+    else {
+        echo "Kode Voucher tidak ditemukan.";
     }
 }
-HitungVoucher("DumbwaysJos", 95000)
+HitungVoucher("DumbwaysJos", 100000)
 ?>
